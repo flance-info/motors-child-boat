d00bca837c98ae09ef9efcc70211489559dc8971
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-08-17 20:50:17 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,28 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', 'stm_enqueue_parent_styles' );
-
 function stm_enqueue_parent_styles() {
 
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('stm-theme-style') );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'stm-theme-style' ) );
 
+}
+
+if ( ! function_exists( 'stm_get_user_child_role' ) ) {
+	function stm_get_user_child_role( $default, $user_id = null ) {
+		$user_data = get_userdata( $user_id ? $user_id : get_current_user_id() );
+
+		if ( ! empty( $user_data ) ) {
+			$roles_to_check = ['stm_dealer', 'subscriber'];
+			$user_roles = $user_data->roles;
+
+			// Check if there's any intersection between user roles and the roles we're checking
+			if ( array_intersect( $roles_to_check, $user_roles ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	add_filter( 'stm_get_user_role', 'stm_get_user_child_role', 20, 2 );
 }
